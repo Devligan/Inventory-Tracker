9182398c1c1db1bce89d8f180f20a551f0baa141
sriharsha challangi <sriharshachallangi@gmail.com>
2026-02-28 12:26:39 -0600

    cleaning

--- Main.cs
+++ Main.cs
@@ -7,8 +7,15 @@ static class Program
     [STAThread]
     static void Main()
     {
-        Application.EnableVisualStyles();
-        Application.SetCompatibleTextRenderingDefault(false);
-        Application.Run(new MainForm());
+        try
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.ToString(), "Startup Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
     }
 }
